e1a94a6505f22603418682400b22d502ce9e7c52
safiee <susu.suanil@gmail.com>
2017-01-13 13:52:23 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -89,7 +89,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'กรูณาถามให้ตรงเงื่อนไข'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
