25d7860a625343074dfab3864e18b4e399841b2d
safiee <susu.suanil@gmail.com>
2017-01-13 13:39:56 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -79,7 +79,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'กรุณากรอดให้ตรงเงือนไข'
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
