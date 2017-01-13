0a01f4d1b49e02d3c119e6e6e1d7bca2eb16f933
safiee <susu.suanil@gmail.com>
2017-01-13 13:53:54 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 
-<title>test</title>
+<title>tests</title>
 
 <meta http-equiv="Content-Language" content="th"> 
 <meta http-equiv="content-Type" content="text/html; charset=window-874"> 
@@ -89,7 +89,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'กรูณาถามให้ตรงเงื่อนไข'
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
